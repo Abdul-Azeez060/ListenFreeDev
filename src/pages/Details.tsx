b03f8575cd5fa8028010f66e04f8cbeacc386582
Abdul-Azeez060 <azeez160604@gmail.com>
2025-03-24 16:17:52 +0530

    optimised the details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -18,6 +18,10 @@ import he from "he";
 import { fetchUserPlaylstSongs } from "@/appwrite/databaseActions";
 import { useCurrentUserData } from "@/context/userContext";
 import LazyImage from "@/components/LazyImage";
+import { FixedSizeList as List } from "react-window"; // Import react-window
+import { AddToPlaylist } from "@/components/AddToPlaylist"; // Import the
+//
+// list component
 
 function Details() {
   const { albumId, playlistId, artistId } = useParams();
@@ -46,31 +50,20 @@ function Details() {
       setImageLoaded(false);
       if (category === "albums") {
         const result = await fetchAlbumSongs(albumId, url);
-        //console.log(result.data, "these are the songs in the album");
         setDetailSongs(result.data);
       } else if (category === "playlists") {
         const result = await fetchPlaylistSongs(playlistId, url);
-        console.log(result.data, "this is playlist data");
-        console.log(result, "this is the result");
         setDetailSongs(result.data);
       } else if (category === "artists") {
         const result = await fetchArtistSongs(artistId);
-        // console.log(result);
         result.data.songs = [...result.data.topSongs];
         setDetailSongs(result.data);
-        // console.log(result.data, "this is hte data");
-
-        setDetailSongs(result.data);
       } else if (category === "userPlaylists") {
-        console.log(playlistId, "This is playlistId");
         const songIds = JSON.parse(
           localStorage.getItem(`playlist:${playlistId}`)
         );
-        console.log(songIds, "these are songsIds");
         const songs = await fetchSongsByIds(songIds);
-        console.log(songs, "these are the playlist songs");
         if (songs) {
-          console.log(playlists, "this is playlists");
           const playlistMetaData = playlists.filter((playlist) => {
             if (playlistId == playlist.$id) {
               return playlist;
@@ -96,25 +89,71 @@ function Details() {
     getSongs();
   }, []);
 
+  // Row renderer for react-window
+  const Row = ({ index, style }) => {
+    const song = detailSongs?.songs[index];
+    return (
+      <motion.div
+        key={song.id}
+        className="flex items-center justify-between space-x-4 p-2 hover:text-black hover:bg-gray-700/60 rounded-lg cursor-pointer"
+        whileHover={{ scale: 1.01 }}
+        style={style}>
+        <div
+          className="flex items-center w-[calc(100vw-5rem)]"
+          onClick={() => {
+            setCurrentSongId(song.id);
+            addSong(song);
+            setSongs(detailSongs.songs);
+          }}>
+          <LazyImage
+            src={song.image[2].url}
+            alt={song.name}
+            className="w-12 mr-3 h-12 rounded-md object-cover"
+          />
+          <div className="w-[11rem] sm:w-[13rem] md:w-[15rem] lg:w-[20rem] xl:w-[35rem]">
+            <h3 className="font-medium truncate text-slate-300">
+              {he.decode(song.name)}
+            </h3>
+            <p className="text-sm text-slate-400 truncate">
+              {he.decode(
+                song?.artists.primary?.map((artist) => artist.name).join(", ")
+              )}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          {/* AddToPlaylist Button */}
+          <div>
+            <AddToPlaylist songId={song.id} />
+          </div>
+
+          {/* PlusCircleIcon Button */}
+          <PlusCircleIcon
+            onClick={() => {
+              addSong(song);
+              toast("Song added to queue");
+            }}
+            className="text-white"
+          />
+        </div>
+      </motion.div>
+    );
+  };
+
   return (
-    <div className="h-screen  scrollbar-hide bg-[#12121e] overflow-auto ">
+    <div className="h-screen scrollbar-hide bg-[#12121e] overflow-hidden">
       {!imageLoaded && (
-        <div className="animate-pulse ">
-          <div className="bg-gray-600  sm:w-[20rem] md:p-10  md:w-[30rem] h-80  md:h-96 md:mt-10 md:mb-5 opacity-80 mx-auto rounded-lg"></div>
+        <div className="animate-pulse">
+          <div className="bg-gray-600 sm:w-[20rem] md:p-10 md:w-[30rem] h-80 md:h-96 md:mt-10 md:mb-5 opacity-80 mx-auto rounded-lg"></div>
         </div>
       )}
       {isLoading ? (
-        <div className="animate-pulse ">
-          {/* Placeholder for Image */}
-
+        <div className="animate-pulse">
           <div className="flex justify-between items-center mt-4 px-5">
-            {/* Placeholder for Text */}
             <div className="w-[80%]">
               <div className="h-4 bg-gray-600 rounded w-3/4 mb-2"></div>
               <div className="h-3 bg-gray-600 rounded w-1/2"></div>
             </div>
-
-            {/* Placeholder for Button */}
             <div className="bg-gray-600 size-14 rounded-full mx-4"></div>
           </div>
         </div>
@@ -133,7 +172,7 @@ function Details() {
               setImageLoaded(true);
             }}
             onError={() => console.log("Failed to load image")}
-            style={{ display: imageLoaded ? "block" : "none" }} // Hide image until fully loaded
+            style={{ display: imageLoaded ? "block" : "none" }}
           />
 
           <div className="flex justify-between items-center">
@@ -149,17 +188,14 @@ function Details() {
                 className="mx-4"
                 onClick={() => {
                   setSongs(detailSongs.songs);
-                  console.log("button clicked");
                   setCurrentSongId(detailSongs?.songs[0].id);
 
                   const playlists = JSON.parse(
                     localStorage.getItem("playlists") || "[]"
                   );
 
-                  // Update the playlists array with the new detailSongs
                   const updatedPlaylists = [...playlists, detailSongs];
 
-                  // Save the updated playlists array back to localStorage
                   localStorage.setItem(
                     "playlists",
                     JSON.stringify(updatedPlaylists)
@@ -172,7 +208,7 @@ function Details() {
         </div>
       )}
 
-      <div className="p-4 mb-40">
+      <div className="p-4 mb-40 scrollbar-hide">
         {isLoading
           ? Array(4)
               .fill(0)
@@ -187,55 +223,16 @@ function Details() {
                   </div>
                 </div>
               ))
-          : detailSongs?.songs?.map((song: Song) => (
-              <motion.div
-                key={song.id}
-                className="flex items-center justify-between space-x-4 p-2 hover:text-black hover:bg-gray-700/60 rounded-lg cursor-pointer"
-                whileHover={{ scale: 1.01 }}>
-                <div
-                  className="flex items-center w-[calc(100vw-5rem)]"
-                  onClick={() => {
-                    // console.log("clicked the song");
-                    setCurrentSongId(song.id);
-                    addSong(song);
-                    setSongs(detailSongs.songs);
-                    // console.log("addded the song to the state");
-                  }}>
-                  {/* <img
-                    src={song.image[2].url}
-                    alt={song.name}
-                    className="w-12 mr-3 h-12 rounded-md object-cover"
-                    loading="lazy"
-                  /> */}
-                  <LazyImage
-                    src={song.image[2].url}
-                    alt={song.name}
-                    className="w-12 mr-3 h-12 rounded-md object-cover"
-                  />
-                  <div className="w-[11rem] sm:w-[13rem] md:w-[15rem] lg:w-[20rem] xl:w-[35rem]">
-                    <h3 className="font-medium  truncate   text-slate-300">
-                      {he.decode(song.name)}
-                    </h3>
-                    <p className="text-sm text-slate-400  truncate ">
-                      {he.decode(
-                        song?.artists.primary
-                          ?.map((artist) => artist.name)
-                          .join(", ")
-                      )}
-                    </p>
-                  </div>
-                </div>
-                <div>
-                  <PlusCircleIcon
-                    onClick={() => {
-                      addSong(song);
-                      toast("Song added to queue");
-                    }}
-                    className="text-white"
-                  />
-                </div>
-              </motion.div>
-            ))}
+          : detailSongs?.songs && (
+              <List
+                height={window.innerHeight - 550} // Adjust height based on screen size
+                itemCount={detailSongs.songs.length}
+                itemSize={80} // Height of each row
+                width="100%"
+                className="scrollbar-hide">
+                {Row}
+              </List>
+            )}
       </div>
     </div>
   );
